cddb1b912f52768fe18f9e5fcdfe3dc75aaf1a72
Adrian Maraj <adrian.maraj@yahoo.de>
2024-08-27 15:46:46 +0200

    changes

--- capture_save_legitimate.py
+++ capture_save_legitimate.py
@@ -3,29 +3,32 @@ from csv import writer
 import zipfile
 from pylookyloo import Lookyloo
 from uuids_legitimate import uuids_amazon, uuids_ameli, uuids_amendes, uuids_atandt, uuids_credit_agricole, \
-    uuids_luxtrust, uuids_microsoft, uuids_netflix, uuids_orange, uuids_paypal
+    uuids_luxtrust, uuids_microsoft, uuids_netflix, uuids_orange, uuids_paypal, other
 
 lookyloo = Lookyloo()
 
 if lookyloo.is_up:
     #name of the institution so that we do not have to change the paths in the code
-    institution = "credit_agricole"
+    institution = "other"
 
     #need to change the uuid list manually
-    for uuid in uuids_credit_agricole:
+    for uuid in other:
 
         #saving the capture in the corresponding folder
         zip_buffer = lookyloo.get_complete_capture(uuid)
         print(uuid)
 
         with zipfile.ZipFile(zip_buffer, 'r') as zip_ref:
-            zip_ref.extractall('./'+ institution +'_legitimate')
+            #zip_ref.extractall('./'+ institution +'_legitimate')
+            zip_ref.extractall('./'+ institution)
             name = zip_ref.namelist()[0].split('/')[0]
         zip_buffer.close()
 
-        txt_datei = './'+ institution +'_legitimate/' + name + '/0.last_redirect.txt'
+        #txt_datei = './'+ institution +'_legitimate/' + name + '/0.last_redirect.txt'
+        txt_datei = './'+ institution +'/' + name + '/0.last_redirect.txt'
 
-        csv_datei = './'+ institution +'_legitimate/'+ institution +'_urls.csv'
+        #csv_datei = './'+ institution +'_legitimate/'+ institution +'_urls.csv'
+        csv_datei = './'+ institution +'/'+ institution +'_urls.csv'
 
         #writing the uuid and the url into the csv file
         with open(txt_datei, 'r') as txt_file, open(csv_datei, 'a', newline='') as csv_file:
@@ -36,7 +39,8 @@ if lookyloo.is_up:
                 daten = zeile.strip().split(',')
                 csv_writer.writerow(daten)
 
-        csv_datei = './'+ institution +'_legitimate/'+ institution +'_takedown_info.csv'
+        #csv_datei = './'+ institution +'_legitimate/'+ institution +'_takedown_info.csv'
+        csv_datei = './'+ institution +'/'+ institution +'_takedown_info.csv'
 
         #takedown information are written into the csv file
         with open(csv_datei, 'a') as f_object:
